40fa43c3b19998f40308c0195528ff31e80e1fd4
noxy17 <ajayjalal2005@gmail.com>
2024-12-02 19:38:02 +0530

    completed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python-chat-app
+
+
+Link of chat app - https://python-chat-app-jlev.onrender.com
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import random
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# python dict. Store connected users. Key is socketid value is username and avatarUrl
+# python dict. Store connected users. Key is socket id, value is username and avatarUrl 
 users = {}
 
 @app.route('/')
@@ -18,8 +18,8 @@ def handle_connect():
     username = f"User_{random.randint(1000,9999)}"
     gender = random.choice(["girl","boy"])
     # https://avatar.iran.liara.run/public/boy?username=User_123
-    avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"
-    
+    avatar_url = f" https://avatar.iran.liara.run/public/{gender}?username={username}"
+
     users[request.sid] = { "username":username,"avatar":avatar_url}
 
     emit("user_joined", {"username":username,"avatar":avatar_url},broadcast=True)
@@ -30,33 +30,29 @@ def handle_connect():
 def handle_disconnect():
     user = users.pop(request.sid, None)
     if user:
-        emit("user_left", {"username":user["username"]},broadcast=True)
+      emit("user_left", {"username":user["username"]},broadcast=True)
 
 
 @socketio.on("send_message")
 def handle_message(data):
     user = users.get(request.sid)
     if user:
-        emit("new_message",{
+        emit("new_message", {
             "username":user["username"],
             "avatar":user["avatar"],
             "message":data["message"]
-            },broadcast=True)
-        
+        }, broadcast=True)
+
 @socketio.on("update_username")
-def handle_update_username_(data):
+def handle_update_username(data):
     old_username = users[request.sid]["username"]
-    new_username = data["new_username"]
+    new_username = data["username"]
     users[request.sid]["username"] = new_username
 
-    emit("username_updated",{
+    emit("username_updated", {
         "old_username":old_username,
         "new_username":new_username
-    },broadcast=True)
-    
-
-
-
+    }, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app) 
